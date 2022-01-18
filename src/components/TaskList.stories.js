de4a951a11053bd2e36e9a8000786ea0d0a8ab52
John Hornsby <john.hornsby@inspiretec.com>
2022-01-18 08:42:24 +0000

    Redux Data

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import TaskList from './TaskList';
-import * as TaskStories from './Task.stories';
+// import TaskList from './TaskList';
+import * as TaskStories from "./Task.stories";
+import { PureTaskList } from "./TaskList";
 
 export default {
-  component: TaskList,
-  title: 'TaskList',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
-}
+  component: PureTaskList,
+  title: "PureTaskList",
+  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+};
 
-const Template = args => <TaskList {...args} />;
+const Template = (args) => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
   ],
+  onPinTask: () => {
+    console.log("onPinTask");
+  },
+  onArchiveTask: () => {
+    console.log("onArchiveTask");
+  },
 };
 
 export const WithPinnedTasks = Template.bind({});
@@ -31,7 +38,7 @@ WithPinnedTasks.args = {
   // Inherited data coming from the Default story.
   tasks: [
     ...Default.args.tasks.slice(0, 5),
-    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
   ],
 };
 
